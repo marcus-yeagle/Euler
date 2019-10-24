1cba6f96c48073ec80dc8b72016f0f81ca3cbe69
Marcus Yeagle <marcus@Marcuss-MacBook-Pro.local>
2019-10-24 14:17:17 -0400

    Updated formatting

--- Problem Solutions/Clojure/prob1.cljs
+++ Problem Solutions/Clojure/prob1.cljs
@@ -1,16 +1,9 @@
 ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 ;; The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
-;; 233168
-
-
-
-(defn mul-3-5 [x]
-    (or (= 0 (mod x 3)) (= 0 (mod x 5))))
-
+;; Answer: 233168
+;;
 (print 
     (reduce + 
-        (filter mul-3-5 
+        (filter (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5)))) 
             (take 1000 (range))))
-    "\n")
-    
-; (print (filter mul-3-5 (range 1 10)) "\n")
+    "\n") 
